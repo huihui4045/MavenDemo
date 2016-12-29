6cc17eb5b290b01b84d9a5b290c8c84e6cf2e3ad
千涯秋瑟 <1476265623@qq.com>
2016-12-30 00:23:41 +0800

    jq请求接口

---
+++ JavaEE/src/WebListener.java
@@ -0,0 +1,24 @@
+/**
+ * Created by molu_ on 2016/12/29.
+ */
+
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+@javax.servlet.annotation.WebListener()
+public class WebListener implements ServletContextListener{
+
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+        System.out.printf("Web应用启动");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+        System.out.printf("Web应用关闭");
+    }
+}
