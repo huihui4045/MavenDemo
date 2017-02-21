c1c7deb601c9299578fd73f3df6af7e3ba8fc359
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2017-02-21 18:25:43 +0800

    Java 数据类型

---
+++ JavaSE/Sort/src/data/DataType.java
@@ -0,0 +1,19 @@
+package data;
+
+/**
+ * Created by gavin on 2017/2/21.
+ */
+public class DataType {
+
+    public static void main(String[] args) {
+
+        Integer a1=1;
+        Integer a2=1;
+
+        Integer b1=200;
+        Integer b2=200;
+
+        System.out.println(a1==a2);
+        System.out.println(b1==b2);
+    }
+}
